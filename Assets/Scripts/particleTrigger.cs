b8fc7d7a7c50a0c2964eb03a845eb09210a51892
Tuna Eskan <tunaeskanaytac@gmail.com>
2024-10-06 01:07:10 +0300

    Resolve Merge

--- Assets/Scripts/particleTrigger.cs
+++ Assets/Scripts/particleTrigger.cs
@@ -1,19 +1,35 @@
+using System.Collections;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TriggerParticle : MonoBehaviour
 {
     public ParticleSystem particleSystem; // Particle System referansı
+    [SerializeField] private GameObject cubeCloseCam;
+    [SerializeField] private GameObject cubeFarCam;
 
-    private void OnCollisionEnter(Collider collision)
+    private void OnCollisionEnter(Collider other)
     {
+        
         // Eğer player Cube objesine dokunduysa
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Cube Collided");
             // Particle System'i başlat
             if (particleSystem != null)
             {
                 particleSystem.Play();
             }
+
+            StartCoroutine(SwitchCam());
         }
+    }
+
+    IEnumerator SwitchCam()
+    {
+        yield return new WaitForSeconds(1f);
+        cubeCloseCam.SetActive(false);
+        cubeFarCam.SetActive(true);
     }
 }
